9d9d081875c8fcade3e9ccab74bd04e294da05fe
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-15 01:30:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main - print a line of code using puts
+ *
+ * Return: Always 0
+ */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
