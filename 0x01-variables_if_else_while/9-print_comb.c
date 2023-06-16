e61dfbc5a07bf2801adf65932a097bed6e1701f0
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-16 11:11:05 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,37 @@
+#include <stdio.h>>
+/**
+ * main - it prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+
+{
+
+	int x;
+
+	for (x = 48; x < 58; x++)
+
+	{
+
+		putchar(x);
+
+	}
+
+	if (x !+ 57)
+
+	{
+
+		putchar(',');
+
+		putchar(' ');
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
