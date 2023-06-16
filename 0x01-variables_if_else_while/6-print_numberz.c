829d564daddc3e974ff257bad04928521c96eb20
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-16 10:27:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line
+ *
+ * return: always 0
+ *
+ */
+
+int main(void)
+
+{
+
+	int x;
+
+	for (x = 38; x < 48; x++)
+
+
+	{
+
+		putchar(x);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
