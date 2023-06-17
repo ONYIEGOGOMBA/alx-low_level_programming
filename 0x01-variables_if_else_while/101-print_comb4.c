587dc07263c2f898b4643ed4bddf97293dcaa542
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-16 19:51:14 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,10 +2,10 @@
 
 /**
  *
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
+ *main - Prints all possible combinations of two different digits,
+ *in ascending order.
  *
- * Return: Always 0.
+ *Return: Always 0.
  */
 int main(void)
 {
