94fcbbd8491503569ab0f7b498670458108d4944
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-20 15:51:02 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,19 +2,18 @@
 /**
  * _abs - it computes the absolute value of an integer.
  *
- * Return: The absolute value of int
+ * Return: Always 0
  *
  */
 
 int _abs(int n)
 
+{
 	if (n >= 0)
-	
-	{
-		return (n);
-	}
 
+		return (n);
 	else
+
 		return (-n);
 
 }
