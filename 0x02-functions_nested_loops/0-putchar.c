2ebf5ddbe3b921cc882253f83ce1123728a8809b
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-20 00:41:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - it prints _putchar, followed by a new line.
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+
+{
+	putchar("_putchar\n");
+
+	return (0);
+
+}
