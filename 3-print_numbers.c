783dd8b5907aa1582010c81be3d52d0fa649fde3
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-22 00:27:41 -0700

    3-print_numbers.c

---
+++ 3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - it prints the numbers,
+ * from 0 to 9, followed by a new line.
+ *
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+
+}
