6ed8aa6a66479b001a39aaf0eafbeaea9531ac58
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-22 02:53:17 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main."
+/**
+ * print_line - it draws a straight line in the terminal.
+ *
+ */
+
+void print_line(int n)
+{
+
+	int line;
+
+	if (n > 0)
+	{
+
+		for (line = o; line < n; line++)
+			_putchar('_');
+	}
+	_putchar('\n');
+
+}
