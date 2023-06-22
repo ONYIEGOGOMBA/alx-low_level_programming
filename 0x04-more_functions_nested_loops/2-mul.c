f2c11631e3275800609c2f3931149a01b589c46e
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-22 04:03:20 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,10 +4,10 @@
  * @a: The first integer to be multiplied.
  * @b: The second integer to be multiplied.
  *
+ * Return: The result of the multiplication.
  */
 
 int mul(int a, int b)
-
 {
 	return (a * b);
 
