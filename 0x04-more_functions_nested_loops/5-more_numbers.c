504cbd48c6d1b54fe5498089ed1de6c4a736855a
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-22 04:12:14 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,6 @@
 /**
  * more_numbers -  it prints 10 times the numbers,
  * from 0 to 14, followed by a new line.
- *
  */
 
 void more_numbers(void)
@@ -10,11 +9,14 @@ void more_numbers(void)
 
 	int num, digit;
 
-	for(digit = 0; digit <= 9; digit++)
+	for (digit = 0; digit <= 9; digit++)
 	{
+
 		for (num = 0; num <= 9; num++)
 		{
+
 			if (num < 9)
+
 				_putchar((num / 10) + '0');
 			_putchar((num % 10) + '0');
 		}
