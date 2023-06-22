41dd74db35f70cef423b4ccf9054c166447f6d85
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-22 00:00:38 -0700

    0-isupper.c

---
+++ 0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - it checks for uppercase character.
+ *
+ * Return: Returns 1 if c is uppercase
+ * Returns 0 otherwise
+ *
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
+
+}
