0318aa546b44cbf39ef325913b55b915a8cf7355
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-22 05:28:45 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,14 @@ void more_numbers(void)
 	for (digit = 0; digit <= 9; digit++)
 	{
 
-		for (num = 0; num <= 9; num++)
-		{
+	for (num = 0; num <= 9; num++)
+	{
 
-			if (num < 9)
+	if (num < 9)
 
-				_putchar((num / 10) + '0');
+		_putchar((num / 10) + '0');
 			_putchar((num % 10) + '0');
-		}
 	}
 	_putchar('\n');
+	}
 }
