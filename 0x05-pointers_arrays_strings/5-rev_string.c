9479289ea019f80448294adc4042b18deafe3f6a
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-26 13:06:25 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,7 +14,7 @@ void rev_string(char *s)
 	while (s[index++])
 		len++;
 
-	for (index = len - 1; index >= len / 2; index==)
+	for (index = len - 1; index >= len / 2; index--)
 	{
 		bog = s[index];
 		s[index] = s[len -index - 1];
