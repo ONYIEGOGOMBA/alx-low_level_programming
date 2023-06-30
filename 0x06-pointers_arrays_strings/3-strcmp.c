8b494f11d0fa395ca1ec806f2b57898cc9eae12e
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-30 09:04:58 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,6 @@
  *
  * @s1: string 1 input to compare
  * @s2: against this other string 2
-<<<<<<< HEAD
  * Return: The given istructions
  *
  */
@@ -27,33 +26,4 @@ int _strcmp(char *s1, char *s2)
 		s++;
 	}
 	return (d);
-=======
- *
- * Return: as instructed
- */
-
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0, r;
-
-	while (s1[i] != '\0' && s2[i] != '\0')
-
-		{
-			if (s1[i] != s2[i])
-
-			{
-				r = s1[i] - s2[i];
-				break;
-			}
-
-			else
-			{
-				r = s1[i] - s2[i];
-			}
-
-			i++;
-		}
-
-	return (r);
->>>>>>> 9833842b25daa5e3265515be1bc7dbebc4d5e179
 }
