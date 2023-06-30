fd80d5ec88fdf3867fac4a1c3fba48e8a1898743
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-06-30 08:15:35 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper  - a function that chages lowercase
+ * letters of a string to uppercase.
+ *
+ * @s: pointer to input char
+ * Return: @s
+ */
+char *string_toupper(char *s)
+{
+        int b = 0;
+
+        while (s[b] != '\0')
+        {
+                if (s[b] >= 97 && s[b] <= 122)
+                        s[b] = s[b] - 32;
+                b++;
+
+        }
+        return (s);
+}
