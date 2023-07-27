2ca3b4e2adcb8150f3f16cbd166078fe21e569d2
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-07-27 11:10:46 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,15 +16,12 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%lu] %s\n", count, h->str);
-		}
-		count += 2;
+
 		h = h->next;
+		count += 2;
 	}
 	printf("[%d] %s\n", h->len, h->str);
 
