f0d139481569fa4a455a3bc5dc6d288d818aca08
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-07-28 00:51:35 -0700

    4. Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,6 +7,12 @@
 void free_list(list_t *head)
 {
 	list_t *current;
+	/**
+	 * while current head is not NULL
+	 * set head as the next node and
+	 * free the current node str then
+	 * free current node.
+	 */
 
 	while ((current = head) != NULL)
 	{
