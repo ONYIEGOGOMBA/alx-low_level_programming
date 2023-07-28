da31bcfede2759a12b0a0dfd9143e0b8edfeae7b
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-07-28 00:36:30 -0700

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - free single list link
+ * @head: pointer to the first node of the list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current != NILL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
