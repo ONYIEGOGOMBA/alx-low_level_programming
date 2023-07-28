7c8bb6e3c454604f9a6d47dda98030f721263dd4
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-07-28 01:10:44 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * premain - Functions that run before main
+ * Return: nothing
+ */
+void __attribute__((constructor)) pre_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
