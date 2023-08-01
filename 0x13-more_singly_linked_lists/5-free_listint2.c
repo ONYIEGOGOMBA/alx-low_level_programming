5d917f5a488fcf45f7727d4c40fb4a713f18d509
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-08-01 03:50:51 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
