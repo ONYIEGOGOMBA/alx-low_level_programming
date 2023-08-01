162b465e3d354c1202550c9549ab18f8de8d7288
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-08-01 05:41:45 -0700

    5. Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
+		current = *head;
+
+		while ((temp = current) != NULL)
+	{
+		current = current->next;
 		free(temp);
+	}
+
+		*head = NULL;
 	}
 }
