0455195c0e70ccfa04051c6f248ca81f6fa70a87
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-08-03 01:56:08 -0700

    4. 100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - Clear bit to 0 at a given index
+ * @n: number
+ * @index: index within binary number
+ * Return: 1 if success, or -1 if error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
+		return (-1);
+
+	unsigned long int mas = 1UL << index;
+
+	*n = *n & (~mas);
+	return (1);
+}
