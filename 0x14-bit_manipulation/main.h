b31d18b0b134a7559f4f542f139abb173aeb4017
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-08-31 05:41:26 -0700

    1. 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,9 @@
 
 #include <stdlib.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 void function1(int arg1, float arg2);
 int function2(char arg);
 void function3();
