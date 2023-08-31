78ad2b7c9c416a2045e92b82d60c06bed2aacc40
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-08-31 05:01:37 -0700

    REABME file

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+THIS IS A PROJECT ON: 0x14. C - Bit manipulation
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+void function1(int arg1, float arg2);
+int function2(char arg);
+void function3();
+
+#endif /* MAIN_H */
