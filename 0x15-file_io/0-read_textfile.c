46ac92adb7e1b26aedfb4bc22dffd9e7d77ec22b
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-09-04 02:03:19 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * read_textfile - reads the textfile
+ * @filename: constant character
+ * @letters: the size
+ * Return: characters read.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int bg;
+	char *bro;
+	ssize_t bytes, b;
+
+	if (!filename)
+		return (0);
+	bg = open(filename, O_RDONLY);
+	if (bg == -1)
+	{
+		close(bg);
+		return (0);
+	}
+
+	bro = malloc(sizeof(char) * letters);
+	if (!bro)
+	{
+		close(bg);
+		return (0);
+	}
+
+	bytes = read(bg, bro, letters);
+
+	if (bytes == -1)
+	{
+		close(bg);
+		free(bro);
+		return (0);
+	}
+
+	b = write(STDOUT_FILENO, bro, bytes);
+
+	if (b == -1)
+	{
+		close(bg);
+		free(bro);
+		return (0);
+	}
+	close(bg);
+	return (bytes);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+int _putchar(char c);
+void *custom_malloc(size_t size);
+void custom_free(void *ptr);
+void custom_exit(int status);
+
+#endif
