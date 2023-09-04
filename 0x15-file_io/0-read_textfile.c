96ff92f9559faa021f8a84962ed64823e5e34846
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-09-04 09:50:37 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,19 +1,19 @@
-#include "main.h"
-#include <stdlib.h>
 #include <unistd.h>
-#include <stddef.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include "main.h"
+
 /**
- * read_textfile - reads the textfile
- * @filename: constant character
- * @letters: the size
- * Return: characters read.
+ * read_textfile - reads and prints from a file
+ * @filename: path to file
+ * @letters: chars to read
+ * Return: chars read
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int bg;
-	char *bro;
-	ssize_t bytes, b;
+	char *buff;
+	ssize_t bites, b;
 
 	if (!filename)
 		return (0);
@@ -24,30 +24,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	bro = malloc(sizeof(char) * letters);
-	if (!bro)
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
 	{
 		close(bg);
 		return (0);
 	}
 
-	bytes = read(bg, bro, letters);
+	bites = read(bg, buff, letters);
 
-	if (bytes == -1)
+	if (bites == -1)
 	{
 		close(bg);
-		free(bro);
+		free(buff);
 		return (0);
 	}
 
-	b = write(STDOUT_FILENO, bro, bytes);
+	b = write(STDOUT_FILENO, buff, bites);
 
 	if (b == -1)
 	{
 		close(bg);
-		free(bro);
+		free(buff);
 		return (0);
 	}
 	close(bg);
-	return (bytes);
+	return (bites);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,11 +3,9 @@
 
 #include <stddef.h>
 
-int _putchar(int c);
+int _putchar(char c);
+int _strlen(const char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
-void *custom_malloc(size_t size);
-void custom_free(void *ptr);
-void custom_exit(int status);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
