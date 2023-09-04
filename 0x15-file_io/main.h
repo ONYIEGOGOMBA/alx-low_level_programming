fd6507c349b33ead26b46444c6e3485213da1731
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-09-04 02:19:11 -0700

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,5 +7,8 @@ int _putchar(char c);
 void *custom_malloc(size_t size);
 void custom_free(void *ptr);
 void custom_exit(int status);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
